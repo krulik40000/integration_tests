8db1409d6ddc2320c2f8e0aac285497ca0f526a7
Paxon96 <bwloczyk@gmail.com>
2019-05-27 17:54:36 +0200

    3.4.1 Add test for trying to add not unique email

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -40,4 +40,19 @@ public class CreateUserTest extends FunctionalTests {
                    .when()
                    .post(USER_API);
     }
+
+    @Test
+    public void postWithNotUniqueEmailReturnsBadRequestTest() {
+        JSONObject jsonObj = new JSONObject().put("email", "john@domain.com");
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .body(jsonObj.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_CONFLICT)
+                   .when()
+                   .post(USER_API);
+    }
 }
