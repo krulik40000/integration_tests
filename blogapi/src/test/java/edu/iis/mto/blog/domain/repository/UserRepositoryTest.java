606cea37c279e8860f46fa2864020083beb05e69
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-02 14:31:54 +0200

    naprawa testów z @Ignore

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/UserRepositoryTest.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,34 +19,27 @@ import edu.iis.mto.blog.domain.model.User;
 @DataJpaTest
 public class UserRepositoryTest {
 
-    @Autowired
-    private TestEntityManager entityManager;
+    @Autowired private TestEntityManager entityManager;
 
-    @Autowired
-    private UserRepository repository;
+    @Autowired private UserRepository repository;
 
     private User user;
 
-    @Before
-    public void setUp() {
+    @Before public void setUp() {
         user = new User();
         user.setFirstName("Jan");
         user.setEmail("john@domain.com");
         user.setAccountStatus(AccountStatus.NEW);
     }
 
-    @Ignore
-    @Test
-    public void shouldFindNoUsersIfRepositoryIsEmpty() {
+    @Test public void shouldFindNoUsersIfRepositoryIsEmpty() {
 
         List<User> users = repository.findAll();
 
         Assert.assertThat(users, Matchers.hasSize(0));
     }
 
-    @Ignore
-    @Test
-    public void shouldFindOneUsersIfRepositoryContainsOneUserEntity() {
+    @Test public void shouldFindOneUsersIfRepositoryContainsOneUserEntity() {
         User persistedUser = entityManager.persist(user);
         List<User> users = repository.findAll();
 
@@ -55,10 +47,7 @@ public class UserRepositoryTest {
         Assert.assertThat(users.get(0).getEmail(), Matchers.equalTo(persistedUser.getEmail()));
     }
 
-    @Ignore
-    @Test
-    public void shouldStoreANewUser() {
-
+    @Test public void shouldStoreANewUser() {
         User persistedUser = repository.save(user);
 
         Assert.assertThat(persistedUser.getId(), Matchers.notNullValue());
