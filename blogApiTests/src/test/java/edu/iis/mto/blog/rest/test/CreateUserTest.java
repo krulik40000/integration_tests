89d7cc80c11201b4b6383ed81fcc158a6e06b233
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-03 00:39:40 +0200

    postFormForCreatingNewUserWithNotUniqueMailReturnsConflict

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -11,8 +11,7 @@ public class CreateUserTest extends FunctionalTests {
 
     private static final String USER_API = "/blog/user";
 
-    @Test
-    public void postFormWithMalformedRequestDataReturnsBadRequest() {
+    @Test public void postFormWithMalformedRequestDataReturnsBadRequest() {
         JSONObject jsonObj = new JSONObject().put("email", "tracy@domain.com");
         RestAssured.given()
                    .accept(ContentType.JSON)
@@ -24,5 +23,19 @@ public class CreateUserTest extends FunctionalTests {
                    .statusCode(HttpStatus.SC_CREATED)
                    .when()
                    .post(USER_API);
+    }
+
+    @Test public void postFormForCreatingNewUserWithNotUniqueMailReturnsConflict() {
+        JSONObject jsonObj = new JSONObject().put("email", "jan@domain.com");
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CONFLICT)
+                .when()
+                .post(USER_API);
     }
 }
