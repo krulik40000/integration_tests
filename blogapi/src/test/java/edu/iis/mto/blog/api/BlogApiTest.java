7350f243113504ed00b47ac024249b5e79ecfcf5
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-03 00:33:51 +0200

    postBlogUserShouldResponseWith409CodeWhenDataIntegrityViolationExceptionIsThrown w BlogApiTest

--- blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/api/BlogApiTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -55,6 +56,17 @@ public class BlogApiTest {
     @Test public void getNotExistingBlogUserShouldReturnCode404() throws Exception {
         Mockito.when(finder.getUserData(0L)).thenThrow(new DomainError(DomainError.USER_NOT_FOUND));
         mvc.perform(get("/blog/user/{id}", 0)).andExpect(status().isNotFound());
+    }
+
+    @Test public void postBlogUserShouldResponseWith409CodeWhenDataIntegrityViolationExceptionIsThrown() throws Exception {
+        UserRequest user = new UserRequest();
+        user.setEmail("john@domain.com");
+        user.setFirstName("John");
+        user.setLastName("Steward");
+        Mockito.when(blogService.createUser(user)).thenThrow(new DataIntegrityViolationException("data integrity exception"));
+        String content = writeJson(user);
+
+        mvc.perform(post("/blog/user").contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON_UTF8).content(content)).andExpect(status().isConflict());
     }
 
     private String writeJson(Object obj) throws JsonProcessingException {
