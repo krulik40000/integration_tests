a6e2b97d48e2ca341516f55eb350893945bac6fe
Natalia Ciosek <n_ciosek@wp.pl>
2019-06-03 22:07:49 +0200

    SearchUserTests

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/SearchUserTests.java
@@ -0,0 +1,102 @@
+package edu.iis.mto.blog.rest.test;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class SearchUserTests extends FunctionalTests {
+
+    private static final String GET_API_START = "/blog/user/";
+    private static final String GET_API_END = "/post";
+
+    @Test public void searchOfPostOfUserShouldReturnPostWithProperLikesAmountWhenPostHasLikes() {
+        String userId = "1";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post("/blog/user/4/like/4");
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("size()", is(1))
+                .and()
+                .body("likesCount", hasItems(1))
+                .when()
+                .get(GET_API_START + userId + GET_API_END);
+    }
+
+    @Test public void searchOfPostOfUserWithNoPostShouldReturnNothing() {
+        String userId = "2";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("size()", is(0))
+                .when()
+                .get(GET_API_START + userId + GET_API_END);
+    }
+
+    @Test public void searchOfPostOfRemovedUserShouldReturnBadRequest() {
+        String userId = "3";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .when()
+                .post(GET_API_START + userId + GET_API_END);
+    }
+
+    @Test public void searchOfPostsOfUserWithMultiplePostShouldReturnProperAmount() {
+        String userId = "4";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("size()", is(2))
+                .when()
+                .get(GET_API_START + userId + GET_API_END);
+    }
+
+    @Test public void searchOfPostOfUserShouldReturnPostWithProperLikesAmountWhenPostHasNoLikes() {
+        String userId = "5";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("size()", is(1))
+                .and()
+                .body("likesCount", hasItems(0))
+                .when()
+                .get(GET_API_START + userId + GET_API_END);
+    }
+}
