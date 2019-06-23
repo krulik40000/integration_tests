3f396f928d7c370a197220a6018132cfc5d3948b
209420 <209420@edu.p.lodz.pl>
2019-06-23 17:20:14 +0200

    blogApiTests FindingPostsTest finding_removed_user_post_should_return_404_error_code()

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FindingPostsTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FindingPostsTest.java
@@ -11,7 +11,7 @@ import static org.hamcrest.CoreMatchers.is;
 public class FindingPostsTest {
 
     private static final String EXISTING_POST = "/blog/user/1/post";
-
+    private static final String FINDING_REMOVED_USER_POST = "/blog/user/4/post";
     @Test
     public void finding_pots_of_existing_user_with_confirmed_status_should_return_valid_number_of_posts() {
         JSONObject jsonObj = new JSONObject();
@@ -28,5 +28,21 @@ public class FindingPostsTest {
                    .body("size()", is(1))
                    .when()
                    .get(EXISTING_POST);
+    }
+    @Test
+    public void finding_removed_user_post_should_return_404_error_code() {
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .body(jsonObj.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_NOT_FOUND)
+                   .when()
+                   .get(FINDING_REMOVED_USER_POST);
+
     }
 }
--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -2,8 +2,9 @@
 insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (account_status, email, first_name) values ('NEW', 'brian@domain.com', 'Brian')
 
-insert into user (id, account_status, email, first_name) values (null, 'CONFIRMED', 'test@domain.com', 'test')
-
+insert into user (account_status, email, first_name) values ('CONFIRMED', 'test@domain.com', 'test')
+insert into user (account_status, email, first_name) values ('REMOVED', 'test2@domain.com', 'test2')
 
 insert into blog_post values (null, 'Test post ', 1)
 insert into blog_post values (null, 'Test post ', 3)
+insert into blog_post values (null, 'Test post removed user ', 4)
