52c6c8cb4d8ff2b6baec3c5095cd58da00e7b6a7
209420 <209420@edu.p.lodz.pl>
2019-06-23 15:36:58 +0200

    blogApiTests CreatePostTest creating_post_by_not_confirmed_user_should_return_conflict()

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreatePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreatePostTest.java
@@ -10,7 +10,7 @@ public class CreatePostTest {
 
 
     private static final String CONFIRMED_USER = "/blog/user/1/post";
-
+    static final String NOT_CONFIRMED_USER = "/blog/user/2/post";
 
     @Test
     public void creating_post_by_confirmed_user_should_create_post() {
@@ -26,5 +26,20 @@ public class CreatePostTest {
                    .statusCode(HttpStatus.SC_CREATED)
                    .when()
                    .post(CONFIRMED_USER);
+    }
+    @Test
+    public void creating_post_by_not_confirmed_user_should_return_conflict() {
+        JSONObject jsonObj = new JSONObject().put("Test", "Test");
+
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .body(jsonObj.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_CONFLICT)
+                   .when()
+                   .post(NOT_CONFIRMED_USER);
     }
 }
