9da1a7b1c9de1479ef2c9141099db79aa5a6853d
209420 <209420@edu.p.lodz.pl>
2019-06-23 16:53:20 +0200

    blogApiTests LikePostTest new_user_should_not_be_able_to_like_post()

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -11,6 +11,7 @@ import java.net.URI;
 public class LikePostTest {
 
     private static final String LIKE_OTHER_USER_POST ="/blog/user/1/like/2" ;
+    private static final String NEW_USER_LIKE_POST ="/blog/user/2/like/1";
     private static final String LIKE_OWN_POST = "/blog/user/1/like/1";
 
     @Test
@@ -43,6 +44,22 @@ public class LikePostTest {
                    .statusCode(HttpStatus.SC_NOT_FOUND)
                    .when()
                    .post(LIKE_OWN_POST);
+    }
+
+    @Test
+    public void new_user_should_not_be_able_to_like_post() {
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .body(jsonObj.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_NOT_FOUND)
+                   .when()
+                   .post(NEW_USER_LIKE_POST);
     }
 
 }
