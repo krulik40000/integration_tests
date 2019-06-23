7dfbfa8bcf9a70728c49028853d2533721b9573a
209420 <209420@edu.p.lodz.pl>
2019-06-23 16:47:06 +0200

    blogApiTests LikePostTest confirmed_user_should_be_able_to_like_other_user_post()

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/LikePostTest.java
@@ -0,0 +1,31 @@
+package edu.iis.mto.blog.rest.test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.net.URI;
+
+public class LikePostTest {
+
+    private static final String LIKE_OTHER_USER_POST ="/blog/user/1/like/2" ;
+
+    @Test
+    public void confirmed_user_should_be_able_to_like_other_user_post() {
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .body(jsonObj.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_OK)
+                   .when()
+                   .post(LIKE_OTHER_USER_POST);
+    }
+
+}
--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -1,3 +1,9 @@
 --this script initiates db for h2 db (used in test profile)
 insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (account_status, email, first_name) values ('NEW', 'brian@domain.com', 'Brian')
+
+insert into user (id, account_status, email, first_name) values (null, 'CONFIRMED', 'test@domain.com', 'test')
+
+
+insert into blog_post values (null, 'Test post ', 1)
+insert into blog_post values (null, 'Test post ', 3)
