8e620179b2b439b170b71aaee0728cbdc60f7b73
209278 <209278@edu.p.lodz.pl>
2019-06-25 20:30:03 +0200

    tests for creating posts

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -40,4 +40,6 @@ public class CreateUserTest extends FunctionalTests {
                 .when()
                 .post(USER_API);
     }
+
+
 }
--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FunctionalTests.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FunctionalTests.java
@@ -1,10 +1,18 @@
 package edu.iis.mto.blog.rest.test;
 
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
 import org.junit.BeforeClass;
 
 import io.restassured.RestAssured;
+import org.junit.Test;
 
 public class FunctionalTests {
+
+
+    private static final String POST_API_START = "/blog/user/";
+    private static final String POST_API_END = "/post";
 
     @BeforeClass
     public static void setup() {
@@ -26,6 +34,54 @@ public class FunctionalTests {
             baseHost = "http://localhost";
         }
         RestAssured.baseURI = baseHost;
+    }
+
+    @Test
+    public void blogPostByConfirmedUser_returnCode201(){
+        String userId = "1";
+        JSONObject jsonObj = new JSONObject().put("entry", "blog post");
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CREATED)
+                .when()
+                .post(POST_API_START + userId + POST_API_END);
+    }
+
+    @Test
+    public void blogPostByNewUser_returnCode403(){
+        String userId = "2";
+        JSONObject jsonObj = new JSONObject().put("entry", "blog post");
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_FORBIDDEN)
+                .when()
+                .post(POST_API_START + userId + POST_API_END);
+    }
+
+    @Test
+    public void blogPostByRemovedUser_returnCode403(){
+        String userId = "3";
+        JSONObject jsonObj = new JSONObject().put("entry", "blog post");
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_FORBIDDEN)
+                .when()
+                .post(POST_API_START + userId + POST_API_END);
     }
 
 }
