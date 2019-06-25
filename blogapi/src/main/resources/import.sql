1d274eed4df92622dfe529ffdbda44887c288554
209278 <209278@edu.p.lodz.pl>
2019-06-25 22:09:36 +0200

    search for user posts

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -8,5 +8,6 @@ insert into user (account_status, email) values ('CONFIRMED', 'bbb@domain.com')
 insert into blog_post values (null, 'test', 4)
 insert into blog_post values (null, 'for test two likes', 4)
 insert into blog_post values (null, 'no likes', 5)
-insert into blog_post values (null, 'post for removed user', 3)
 insert into blog_post values (null, 'post for user with id 1', 1)
+insert into blog_post values (null, 'post for removed user', 3)
+
