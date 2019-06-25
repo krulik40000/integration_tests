e147b92f771a5c845f39f516f358e6d8ff76266b
stepaniuk.artur <211650@edu.p.lodz.pl>
2019-06-25 22:19:22 +0200

    3.4.4 added tests for checking posts and likes

--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -6,3 +6,5 @@ insert into user (account_status, email, first_name, last_name) values ('CONFIRM
 
 insert into blog_post values (null, 'Test post by confirmed user', 1)
 insert into blog_post values (null, 'Test post by another confirmed user', 4)
+
+insert into like_post values (null, 2, 1)
