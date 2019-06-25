0c1586cbb120a8e99f41f1484c6719d3773a2454
stepaniuk.artur <211650@edu.p.lodz.pl>
2019-06-25 19:10:20 +0200

    3.2 test for adding like by correct user added

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,5 +1,9 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
@@ -18,12 +22,24 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.Optional;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
 
     @MockBean
     UserRepository userRepository;
+
+    @MockBean
+    BlogPostRepository blogPostRepository;
+
+    @MockBean
+    LikePostRepository likePostRepository;
 
     @Autowired
     BlogDataMapper dataMapper;
@@ -35,9 +51,39 @@ public class BlogManagerTest {
     public void creatingNewUserShouldSetAccountStatusToNEW() {
         blogService.createUser(new UserRequest("John", "Steward", "john@domain.com"));
         ArgumentCaptor<User> userParam = ArgumentCaptor.forClass(User.class);
-        Mockito.verify(userRepository).save(userParam.capture());
+        verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
     }
+
+    @Test
+    public void userWithStatusConfirmedShouldBeAbleToLikePost() {
+        User user = new User();
+        user.setAccountStatus(AccountStatus.CONFIRMED);
+        user.setFirstName("john");
+        user.setId(1L);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        User postOwner = new User();
+        postOwner.setAccountStatus(AccountStatus.CONFIRMED);
+        postOwner.setFirstName("Peter");
+        postOwner.setId(2L);
+        when(userRepository.findById(2L)).thenReturn(Optional.of(postOwner));
+
+        BlogPost post = new BlogPost();
+        post.setUser(postOwner);
+        post.setEntry("about cats again!");
+        post.setId(1L);
+        when(blogPostRepository.findById(1L)).thenReturn(Optional.of(post));
+
+        blogService.addLikeToPost(user.getId(), post.getId());
+
+        ArgumentCaptor<LikePost> likePostArgumentCaptor = ArgumentCaptor.forClass(LikePost.class);
+        verify(likePostRepository).save(likePostArgumentCaptor.capture());
+        LikePost like = likePostArgumentCaptor.getValue();
+        Assert.assertThat(like.getUser(), is(user));
+    }
+
+    
 
 }
