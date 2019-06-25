38c13ebb5906f021409e925a2e345af9285d62ca
209278 <209278@edu.p.lodz.pl>
2019-06-25 20:59:25 +0200

    tests for adding likes

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FunctionalTests.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/FunctionalTests.java
@@ -2,6 +2,7 @@ package edu.iis.mto.blog.rest.test;
 
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
 import org.json.JSONObject;
 import org.junit.BeforeClass;
 
@@ -11,7 +12,7 @@ import org.junit.Test;
 public class FunctionalTests {
 
 
-    private static final String POST_API_START = "/blog/user/";
+    private static final String API_START = "/blog/user/";
     private static final String POST_API_END = "/post";
 
     @BeforeClass
@@ -49,7 +50,7 @@ public class FunctionalTests {
                 .all()
                 .statusCode(HttpStatus.SC_CREATED)
                 .when()
-                .post(POST_API_START + userId + POST_API_END);
+                .post(API_START + userId + POST_API_END);
     }
 
     @Test
@@ -65,7 +66,7 @@ public class FunctionalTests {
                 .all()
                 .statusCode(HttpStatus.SC_FORBIDDEN)
                 .when()
-                .post(POST_API_START + userId + POST_API_END);
+                .post(API_START + userId + POST_API_END);
     }
 
     @Test
@@ -81,7 +82,98 @@ public class FunctionalTests {
                 .all()
                 .statusCode(HttpStatus.SC_FORBIDDEN)
                 .when()
-                .post(POST_API_START + userId + POST_API_END);
+                .post(API_START + userId + POST_API_END);
+    }
+
+    @Test
+    public void likeBlogPostByConfirmedUser_returnCode201() {
+        String userId = "1";
+        String postId = "1";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post(API_START + userId + "/like/" + postId);
+    }
+
+    @Test
+    public void twoLikesByOneUser_stayOneLike() {
+        String userId = "1";
+        String postId = "2";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post(API_START + userId + "/like/" + postId);
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK)
+                .when()
+                .post(API_START + userId + "/like/" + postId);
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .when()
+                .get("/blog/user/4/post")
+                .then()
+                .body("likesCount", Matchers.hasItem(1));
+    }
+
+    @Test
+    public void likeBlogPostByNewUser_returnCode403() {
+        String userId = "2";
+        String postId = "1";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_FORBIDDEN)
+                .when()
+                .post(API_START + userId + "/like/" + postId);
+    }
+
+    @Test
+    public void likeBlogPostByRemovedUser_returnCode403() {
+        String userId = "3";
+        String postId = "1";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_FORBIDDEN)
+                .when()
+                .post(API_START + userId + "/like/" + postId);
+    }
+
+    @Test
+    public void likeBlogPostByOwner_returnCode403() {
+        String userId = "4";
+        String postId = "1";
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_FORBIDDEN)
+                .when()
+                .post(API_START + userId + "/like/" + postId);
     }
 
 }
--- blogapi/src/main/resources/import.sql
+++ blogapi/src/main/resources/import.sql
@@ -2,3 +2,6 @@
 insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'john@domain.com', 'John', 'Steward')
 insert into user (account_status, email, first_name) values ('NEW', 'brian@domain.com', 'Brian')
 insert into user (account_status, email) values ('REMOVED', 'test@domain.com')
+insert into user (account_status, email) values ('CONFIRMED', 'aaa@domain.com')
+insert into blog_post values (null, 'test', 4)
+insert into blog_post values (null, 'for test two likes', 4)
