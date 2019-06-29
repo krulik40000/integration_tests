401403bc2a8bfc8238965a0709d796111dcefa57
Kovalyk.Volodymyr <211646@edu.p.lodz.pl>
2019-06-29 21:47:07 +0200

    3.4.3 add tests for valid and invalid user create a post

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreatePostTest.java
@@ -0,0 +1,45 @@
+package edu.iis.mto.blog.rest.test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class CreatePostTest extends FunctionalTests {
+
+    private static final String VALID_POST_API = "/blog/user/1/post";
+    private static final String INVALID_POST_API = "/blog/user/2/post";
+
+    @Test
+    public void creatingPostWithUserHavingStatusConfirmedSucceeds() {
+        JSONObject jsonObj = new JSONObject().put("entry", "Test");
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CREATED)
+                .when()
+                .post(VALID_POST_API);
+    }
+
+    @Test
+    public void creatingPostWithUserHavingStatusNewFailsWith409ConflictCode() {
+        JSONObject jsonObj = new JSONObject().put("Test", "Test");
+
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json;charset=UTF-8")
+                .body(jsonObj.toString())
+                .expect()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_CONFLICT)
+                .when()
+                .post(INVALID_POST_API);
+    }
+}
