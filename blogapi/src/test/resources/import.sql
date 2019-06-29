368a5098383a5826c629a60ec00e625561cc2377
Kovalyk.Volodymyr <211646@edu.p.lodz.pl>
2019-06-29 21:39:05 +0200

    add insetr Sql

--- blogapi/src/test/resources/import.sql
+++ blogapi/src/test/resources/import.sql
@@ -1,1 +1,7 @@
 --this script initiates db for integration tests 
+insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'john@domain.com', 'John', 'Steward')
+insert into user (account_status, email, first_name) values ('NEW', 'brian@domain.com', 'Brian')
+insert into user (account_status, email, first_name) values ('REMOVED', 'braindead@domain.com', 'poorhim')
+insert into user (account_status, email, first_name, last_name) values ('CONFIRMED', 'peter@domain.com', 'Peter', 'Pen')
+insert into blog_post values (null, 'Test post by confirmed user', 1)
+insert into blog_post values (null, 'Test post by another confirmed user', 4)
